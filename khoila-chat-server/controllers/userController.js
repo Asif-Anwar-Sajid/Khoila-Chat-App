95e97099873b1e77482ffd97ef5438aee50e3834
Asif Anwar Sajid <asif.anwar.sajid@gmail.com>
2024-06-04 01:15:25 +0600

    create login & signup functionality using JWT and BCrypt

--- khoila-chat-server/controllers/userController.js
+++ khoila-chat-server/controllers/userController.js
@@ -2,14 +2,33 @@ const express = require("express");
 const UserModel = require('../models/userModel');
 const expressAsyncHandler = require("express-async-handler");
 const generateToken = require('../Config/generateToken');
-const loginController = () => {};
+
+// Login
+const loginController = expressAsyncHandler(async (req, res) => {
+    const { name, password } = req.body;
+    const user = await UserModel.findOne({name});
+
+    if(user && (await user.matchPassword(password))) {
+        res.status(201).json({
+            _id : user.id,
+            name : user.name,
+            email : user.email,
+            isAdmin : user.isAdmin,
+            token : generateToken(user._id)
+        })
+    } 
+    else {
+        res.status(400).json({ message: "Invalid Username or Password" });
+    }
+});
+
+//Registration
 const registerController = expressAsyncHandler(async (req, res) => {
     const {name, email, password} = req.body;
 
     // chcek for all fields
     if(!name || !email || !password) {
-        res.send(400);
-        throw Error("All necessary input fields have not been filled");
+        res.status(400).json({ message: "All necessary input fields have not been filled" });
     }
 
     // pre-existing user
@@ -39,4 +58,19 @@ const registerController = expressAsyncHandler(async (req, res) => {
     }
 });
 
-module.exports = {loginController, registerController}
+const fetchAllUsersController = expressAsyncHandler(async (req, res) => {
+    const keyword = req.query.search 
+        ? {
+            $or: [
+                { name : { $regex: req.query.search, $options: "i" } },
+                { email : { $regex: req.query.search, $options: "i" } },
+            ],
+        }
+        : {};
+    const users = (await UserModel.find(keyword)).find({
+        _id : { $ne : req.user._id},
+    });
+    res.send(users);
+})
+
+module.exports = {loginController, registerController, fetchAllUsersController}
--- khoila-chat-server/routes/userRouters.js
+++ khoila-chat-server/routes/userRouters.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { loginController, registerController } = require("../controllers/userController");
+const { loginController, registerController, fetchAllUsersController } = require("../controllers/userController");
 const Router = express.Router();
+
+const { protect } = require('../middleware/authMiddleware');
 
 Router.post("/login", loginController);
 Router.post("/register", registerController);
+Router.get("/features", protect, fetchAllUsersController);
 
 module.exports = Router;
