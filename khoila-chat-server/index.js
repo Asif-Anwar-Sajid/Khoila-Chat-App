0c275b3b3700045987a2ac68a15e0a0076373ce9
Asif Anwar Sajid <asif.anwar.sajid@gmail.com>
2024-06-03 21:22:30 +0600

    connect mongoDB

--- khoila-chat-server/index.js
+++ khoila-chat-server/index.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 dotenv.config();
+
+console.log('Mongo URI:', process.env.MONGO_URI);
+
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log("server is connected to DB");
+    } catch(err) {
+        console.log("server is not connected to DB", err.message);
+    }
+};
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send("API is running");
 });
 
-const PORT = process.env.PORT || 5000
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server is Running at port ${PORT}...`));
